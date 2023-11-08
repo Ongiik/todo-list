53ceeb29da5d86b1bdfd0148e3ba89081f40db63
Ongii7 <ongii.k18@gmail.com>
2023-11-07 23:22:59 -0800

    filled out task functions for functionality.

--- tasks.js
+++ tasks.js
@@ -1,48 +1,45 @@
 class Task {
     constructor(title, desc, dueDate, completion, taskIndex) {
+        this.title = title;
+        this.desc = desc;
+        this.dueDate = dueDate;
+        this.completion = completion;
+        this.taskIndex = taskIndex;
+    }
 
+    //Read/Get Task details function
+    readTask = () => {
+        return [this.title, this.desc, this.dueDate, this.completion, this.taskIndex];
+    }
+
+    //Task update functions
+    updateTitle = (title) => {
+        this.title = title;
+    }
+
+    updateDesc = (desc) => {
+        this.desc = desc;
+    }
+
+    updateDue = (dueDate) => {
+        this.dueDate = dueDate
+    }
+
+    updateCompletion = (completion) => {
+        this.completion = completion
+    }
+
+    updateIndex = (taskIndex) => {
+        this.taskIndex = taskIndex;
+    }
+    //Remove task
+    deleteTask = () => {
+        this.title = null;
+        this.desc = null;
+        this.dueDate = null;
+        this.completion = null;
+        this.taskIndex = null;
     }
 }
 
-//Initialize a task for a project
-const createTask = (title, desc, dueDate, completion, taskIndex) => {
-    taskItem = new Task(title, desc, dueDate, completion, taskIndex);
-    return taskItem;
-}
-
-//Read/Get Task details function
-const readTask = (task) => {
-    return [task.title, task.desc, task.dueDate, task.completion, task.taskIndex];
-}
-
-//Task update functions
-const updateTitle = (task, title) => {
-    task.title = title;
-    return task;
-}
-
-const updateDesc = (task, desc) => {
-    task.desc = desc;
-    return task;
-}
-
-const updateDue = (task, dueDate) => {
-    task.dueDate = dueDate;
-    return task;
-}
-
-const updateCompletion = (task, completion) => {
-    task.completion = completion;
-    return task;
-}
-
-const updateIndex = (task, taskIndex) => {
-    task.taskIndex = taskIndex;
-    return task;
-}
-//Remove task
-const deleteTask = (taskItem) => {
-    taskItem = null;
-}
-
-export { createTask, readTask, updateTitle, deleteTask }
+export { Task }
