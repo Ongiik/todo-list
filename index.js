48554aed1abcf9afd8cb3a96ad8fa967fadd514e
Ongiik <benen.k18@gmail.com>
2023-11-09 22:45:24 -0800

    added test for task module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Project } from "./projects.js";
+import { Task } from "./tasks.js";
 
 const defaultProject = new Project("test", "this is a description", 0);
 
@@ -13,3 +14,7 @@ console.log(defaultProject.readProject());
 defaultProject.deleteProject();
 
 console.log(defaultProject.readProject());
+
+const testTask = new Task("test", "this is a description", "11/09", true, "0")
+
+testTask.readTask();
